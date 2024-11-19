06a144cda73be837d025a8bc45a75c77ecf1f598
TomOliver <TheFuzzyMole@hotmail.com>
2024-11-19 08:09:39 +0000

    Added screen width and height to global commons file

--- LowLevelGameplay/src/Core/Commons.h
+++ LowLevelGameplay/src/Core/Commons.h
@@ -4,6 +4,9 @@
 #define PHYSICS_SOLVER_ITERATIONS 5
 #define UNIT_SCALEFACTOR 100
 
+#define SCREEN_WIDTH 1800
+#define SCREEN_HEIGHT 900
+
 extern std::condition_variable_any EndOfFrame;
 extern std::condition_variable_any NextUpdate;
 extern std::condition_variable_any NextFixedUpdate;
